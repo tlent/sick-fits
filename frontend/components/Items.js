97842f00e5e079f47254c32dbc6605181294f9e2
Thomas Lent <tcl293@gmail.com>
2018-10-23 13:23:20 -0400

    Complete #23 Pagination and Cache Invalidation

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -4,10 +4,11 @@ import gql from "graphql-tag";
 import styled from "styled-components";
 import Item from "./Item";
 import Pagination from "./Pagination";
+import { perPage } from "../config";
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -32,10 +33,14 @@ const ItemsList = styled.div`
 
 class Items extends Component {
   render() {
+    const { page } = this.props;
     return (
       <Center>
-        <Pagination page={this.props.page} />
-        <Query query={ALL_ITEMS_QUERY}>
+        <Pagination page={page} />
+        <Query
+          query={ALL_ITEMS_QUERY}
+          variables={{ skip: (page - 1) * perPage }}
+        >
           {({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error: {error.message}</p>;
@@ -48,7 +53,7 @@ class Items extends Component {
             );
           }}
         </Query>
-        <Pagination page={this.props.page} />
+        <Pagination page={page} />
       </Center>
     );
   }
