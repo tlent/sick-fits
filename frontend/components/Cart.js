66c6ba91d5a5ad18d2b728db88bbd055b8b88bc0
Thomas Lent <tcl293@gmail.com>
2018-11-04 22:01:16 -0500

    Complete #39 Apollo Local State Queries and Mutations

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,23 +1,47 @@
-import React, { Component } from "react";
+import { Query, Mutation } from "react-apollo";
+import gql from "graphql-tag";
 import CartStyles from "./styles/CartStyles";
 import Supreme from "./styles/Supreme";
 import CloseButton from "./styles/CloseButton";
 import SickButton from "./styles/SickButton";
 
+const LOCAL_STATE_QUERY = gql`
+  query {
+    cartOpen @client
+  }
+`;
+
+const TOGGLE_CART_MUTATION = gql`
+  mutation {
+    toggleCart @client
+  }
+`;
+
 const Cart = () => {
   return (
-    <CartStyles open>
-      <header>
-        <CloseButton title="close">&times;</CloseButton>
-        <Supreme>Your Cart</Supreme>
-        <p>You have x items in your cart.</p>
-      </header>
-      <footer>
-        <p>$xx.xx</p>
-        <SickButton>Checkout</SickButton>
-      </footer>
-    </CartStyles>
+    <Mutation mutation={TOGGLE_CART_MUTATION}>
+      {toggleCart => (
+        <Query query={LOCAL_STATE_QUERY}>
+          {({ data }) => (
+            <CartStyles open={data.cartOpen}>
+              <header>
+                <CloseButton title="close" onClick={toggleCart}>
+                  &times;
+                </CloseButton>
+                <Supreme>Your Cart</Supreme>
+                <p>You have x items in your cart.</p>
+              </header>
+              <footer>
+                <p>$xx.xx</p>
+                <SickButton>Checkout</SickButton>
+              </footer>
+            </CartStyles>
+          )}
+        </Query>
+      )}
+    </Mutation>
   );
 };
 
 export default Cart;
+export { LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION };
--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,18 +1,34 @@
-import withApollo from 'next-with-apollo';
-import ApolloClient from 'apollo-boost';
-import { endpoint } from '../config';
+import withApollo from "next-with-apollo";
+import ApolloClient from "apollo-boost";
+import { endpoint } from "../config";
+import { LOCAL_STATE_QUERY } from "../components/Cart";
 
 function createClient({ headers }) {
   return new ApolloClient({
-    uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+    uri: process.env.NODE_ENV === "development" ? endpoint : endpoint,
     request: operation => {
       operation.setContext({
         fetchOptions: {
-          credentials: 'include',
+          credentials: "include"
         },
-        headers,
+        headers
       });
     },
+    clientState: {
+      resolvers: {
+        Mutation: {
+          toggleCart(_, variables, { cache }) {
+            const { cartOpen } = cache.readQuery({ query: LOCAL_STATE_QUERY });
+            const data = { data: { cartOpen: !cartOpen } };
+            cache.writeData(data);
+            return data;
+          }
+        }
+      },
+      defaults: {
+        cartOpen: true
+      }
+    }
   });
 }
 
