04ae398d9b6fd766a1bc9fbc888fc7c69f459e30
Thomas Lent <tcl293@gmail.com>
2018-11-06 15:13:28 -0500

    Complete #43 Optimistic Response && Cache Updates with Apollo

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -28,11 +28,22 @@ class RemoveFromCart extends Component {
   static propTypes = {
     id: PropTypes.string.isRequired
   };
+  update = (cache, payload) => {
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+    const cartItemID = payload.data.removeFromCart.id;
+    data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemID);
+    cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+  };
   render() {
     return (
       <Mutation
         mutation={REMOVE_FROM_CART_MUTATION}
         variables={{ id: this.props.id }}
+        update={this.update}
+        optimisticResponse={{
+          __typename: "Mutation",
+          removeFromCart: { __typename: "CartItem", id: this.props.id }
+        }}
       >
         {(removeFromCart, { loading, error }) => (
           <BigButton
