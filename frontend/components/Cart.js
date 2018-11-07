f0b3c57b6e10be6067cabf9215dc9d825dfe4b25
Thomas Lent <tcl293@gmail.com>
2018-11-07 15:35:34 -0500

    Complete #46 Cleaning Up This Render Prop Mess

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,5 +1,6 @@
 import { Query, Mutation } from "react-apollo";
 import gql from "graphql-tag";
+import { adopt } from "react-adopt";
 import User from "./User";
 import CartItem from "./CartItem";
 import calcTotalPrice from "../lib/calcTotalPrice";
@@ -21,49 +22,51 @@ const TOGGLE_CART_MUTATION = gql`
   }
 `;
 
+const Composed = adopt({
+  user: ({ render }) => <User>{render}</User>,
+  toggleCart: ({ render }) => (
+    <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>
+  ),
+  localState: ({ render }) => <Query query={LOCAL_STATE_QUERY}>{render}</Query>
+});
+
 const Cart = () => {
   return (
-    <User>
-      {({ data: { me } }) => {
+    <Composed>
+      {({ user, toggleCart, localState }) => {
+        const { me } = user.data;
+        const { cartOpen } = localState.data;
         if (!me) return null;
         return (
-          <Mutation mutation={TOGGLE_CART_MUTATION}>
-            {toggleCart => (
-              <Query query={LOCAL_STATE_QUERY}>
-                {({ data }) => (
-                  <CartStyles open={data.cartOpen}>
-                    <header>
-                      <CloseButton title="close" onClick={toggleCart}>
-                        &times;
-                      </CloseButton>
-                      <Supreme>
-                        {`${me.name}'${
-                          me.name.slice(-1).toLowerCase() === "s" ? "" : "s"
-                        }`}{" "}
-                        Cart
-                      </Supreme>
-                      <p>
-                        You have {me.cart.length} item
-                        {me.cart.length === 1 ? "" : "s"} in your cart.
-                      </p>
-                    </header>
-                    <ul>
-                      {me.cart.map(cartItem => (
-                        <CartItem cartItem={cartItem} key={cartItem.id} />
-                      ))}
-                    </ul>
-                    <footer>
-                      <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-                      <SickButton>Checkout</SickButton>
-                    </footer>
-                  </CartStyles>
-                )}
-              </Query>
-            )}
-          </Mutation>
+          <CartStyles open={cartOpen}>
+            <header>
+              <CloseButton title="close" onClick={toggleCart}>
+                &times;
+              </CloseButton>
+              <Supreme>
+                {`${me.name}'${
+                  me.name.slice(-1).toLowerCase() === "s" ? "" : "s"
+                }`}{" "}
+                Cart
+              </Supreme>
+              <p>
+                You have {me.cart.length} item
+                {me.cart.length === 1 ? "" : "s"} in your cart.
+              </p>
+            </header>
+            <ul>
+              {me.cart.map(cartItem => (
+                <CartItem cartItem={cartItem} key={cartItem.id} />
+              ))}
+            </ul>
+            <footer>
+              <p>{formatMoney(calcTotalPrice(me.cart))}</p>
+              <SickButton>Checkout</SickButton>
+            </footer>
+          </CartStyles>
         );
       }}
-    </User>
+    </Composed>
   );
 };
 
