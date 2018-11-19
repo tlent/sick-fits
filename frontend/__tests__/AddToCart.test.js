08f9734aefa2a2cf2961c2a3b67928b0a0ec6c7c
Thomas Lent <tcl293@gmail.com>
2018-11-19 13:21:09 -0500

    Complete #64 Testing our Cart

---
+++ frontend/__tests__/AddToCart.test.js
@@ -0,0 +1,88 @@
+import { mount } from "enzyme";
+import wait from "waait";
+import toJSON from "enzyme-to-json";
+import { MockedProvider } from "react-apollo/test-utils";
+import { ApolloConsumer } from "react-apollo";
+import AddToCart, { ADD_TO_CART_MUTATION } from "../components/AddToCart";
+import { fakeUser, fakeCartItem } from "../lib/testUtils";
+import { CURRENT_USER_QUERY } from "../components/User";
+
+const mocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: { ...fakeUser(), cart: [] } } }
+  },
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: { ...fakeUser(), cart: [fakeCartItem()] } } }
+  },
+  {
+    request: { query: ADD_TO_CART_MUTATION, variables: { itemID: "abc123" } },
+    result: {
+      data: {
+        addToCart: {
+          ...fakeCartItem(),
+          quantity: 1
+        }
+      }
+    }
+  }
+];
+
+describe("AddToCart component", () => {
+  it("renders and matches snapshot", async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <AddToCart id={"abc123"} />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    expect(toJSON(wrapper.find("button"))).toMatchSnapshot();
+  });
+
+  it("adds an item to cart when clicked", async () => {
+    let apolloClient;
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <ApolloConsumer>
+          {client => {
+            apolloClient = client;
+            return <AddToCart id={"abc123"} />;
+          }}
+        </ApolloConsumer>
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    let me = (await apolloClient.query({
+      query: CURRENT_USER_QUERY
+    })).data.me;
+    expect(me.cart).toHaveLength(0);
+    wrapper.find("button").simulate("click");
+    await wait();
+    wrapper.update();
+    me = (await apolloClient.query({
+      query: CURRENT_USER_QUERY
+    })).data.me;
+    expect(me.cart).toHaveLength(1);
+    expect(me.cart[0].id).toBe("omg123");
+    expect(me.cart[0].quantity).toBe(3);
+  });
+
+  it("changes from add to adding when clicked", async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <AddToCart id={"abc123"} />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    expect(wrapper.text()).toContain("Add to Cart");
+
+    wrapper.find("button").simulate("click");
+    await wait();
+    wrapper.update();
+    expect(wrapper.text()).toContain("Adding to Cart");
+  });
+});
---
+++ frontend/__tests__/RemoveFromCart.test.js
@@ -0,0 +1,73 @@
+import { mount } from "enzyme";
+import toJSON from "enzyme-to-json";
+import wait from "waait";
+import RemoveFromCart, {
+  REMOVE_FROM_CART_MUTATION
+} from "../components/RemoveFromCart";
+import { CURRENT_USER_QUERY } from "../components/User";
+import { MockedProvider } from "react-apollo/test-utils";
+import { fakeUser, fakeCartItem } from "../lib/testUtils";
+import { ApolloConsumer } from "react-apollo";
+
+const mocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: {
+      data: {
+        me: {
+          ...fakeUser(),
+          cart: [{ ...fakeCartItem(), id: "abc123" }, fakeCartItem()]
+        }
+      }
+    }
+  },
+  {
+    request: { query: REMOVE_FROM_CART_MUTATION, variables: { id: "abc123" } },
+    result: {
+      data: {
+        removeFromCart: {
+          __typename: "CartItem",
+          id: "abc123"
+        }
+      }
+    }
+  }
+];
+
+describe("RemoveFromCart component", () => {
+  it("renders and matches snapshot", () => {
+    const wrapper = mount(
+      <MockedProvider>
+        <RemoveFromCart id={"abc123"} />
+      </MockedProvider>
+    );
+    expect(toJSON(wrapper.find("button"))).toMatchSnapshot();
+  });
+
+  it("removes item from cart on click", async () => {
+    let apolloClient;
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <ApolloConsumer>
+          {client => {
+            apolloClient = client;
+            return <RemoveFromCart id={"abc123"} />;
+          }}
+        </ApolloConsumer>
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    let cart = (await apolloClient.query({ query: CURRENT_USER_QUERY })).data.me
+      .cart;
+    expect(cart).toHaveLength(2);
+
+    wrapper.find("button").simulate("click");
+    await wait();
+    wrapper.update();
+    cart = (await apolloClient.query({ query: CURRENT_USER_QUERY })).data.me
+      .cart;
+    expect(cart).toHaveLength(1);
+    expect(cart[0].id).toBe("omg123");
+  });
+});
