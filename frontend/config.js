40e5595144e44800da3e0056e3d31066fd7ef997
Thomas Lent <tcl293@gmail.com>
2018-11-20 13:14:33 -0500

    Fix production endpoint for now.sh

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
-export const productionEndpoint =
-  "https://sickfits-yoga-prod-tcl.herokuapp.com/";
+export const productionEndpoint = "https://sickfits-backend.now.sh/";
 export const perPage = 4;
