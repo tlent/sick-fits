f5f5d786bdefce66d8ade02b796a35866ca5520d
Thomas Lent <tcl293@gmail.com>
2018-11-20 12:23:48 -0500

    Add production URL (part of #68)

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
+export const productionEndpoint =
+  "https://sickfits-yoga-prod-tcl.herokuapp.com/";
 export const perPage = 4;
